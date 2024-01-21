2bb070f2d9d66d6108d67d86338294c7438e7f1e
Ugo Pattacini <ugo.pattacini@iit.it>
2024-01-21 12:50:32 +0100

    open reqs at startup

--- scripts/cleanup.m
+++ scripts/cleanup.m
@@ -1,5 +1,11 @@
+% Close all open models discarding changes
+bdclose('all');
+
 % Clear test file from Test Manager
 sltest.testmanager.clear;
 
 % Close Test Manager
 sltest.testmanager.close;
+
+% Close Requirement sets and editor discarding changes
+slreq.clear();
