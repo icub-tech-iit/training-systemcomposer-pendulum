e99bfcd56af998d443c4b8a1c148ab7793b32d6b
Ugo Pattacini <ugo.pattacini@iit.it>
2024-03-28 15:17:26 +0100

    discard pending changes to dictionaries upon closure

--- scripts/cleanup.m
+++ scripts/cleanup.m
@@ -1,4 +1,7 @@
-% Close all open models discarding changes
+% Copyright (C) 2024 Fondazione Istitito Italiano di Tecnologia (IIT)
+% All Rights Reserved.
+
+% Close all open models discarding pending changes
 bdclose('all');
 
 % Clear test file from Test Manager
@@ -10,5 +13,8 @@ sltest.testmanager.clearResults;
 % Close Test Manager
 sltest.testmanager.close;
 
-% Close Requirement sets and editor discarding changes
+% Close Requirement sets and editor discarding pending changes
 slreq.clear();
+
+% Close all dictionaries discarding pending changes
+Simulink.data.dictionary.closeAll('-discard');
