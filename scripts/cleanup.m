49eb1a6bdb5b480e44025a081e1f0ecb15703f37
Ugo Pattacini <ugo.pattacini@iit.it>
2024-06-21 18:41:39 +0200

    improvements

--- scripts/cleanup.m
+++ scripts/cleanup.m
@@ -1,9 +1,6 @@
 % Copyright (C) 2024 Fondazione Istitito Italiano di Tecnologia (IIT)
 % All Rights Reserved.
 
-% Close all open models discarding pending changes
-bdclose('all');
-
 % Clear test file from Test Manager
 sltest.testmanager.clear;
 
@@ -15,6 +12,9 @@ sltest.testmanager.close;
 
 % Close Requirement sets and editor discarding pending changes
 slreq.clear();
+
+% Close all open models discarding pending changes
+bdclose('all');
 
 % Close all dictionaries discarding pending changes
 Simulink.data.dictionary.closeAll('-discard');
--- scripts/run_mech_explorer.m
+++ scripts/run_mech_explorer.m
@@ -4,40 +4,40 @@ function run_mech_explorer(pendulum_type, obstacle_enable, varargin)
 %#ok<*AGROW>
 %#ok<*NASGU>
 
-if nargin == 2
-    S1 = dir('./cache');
-    S1 = S1(~[S1.isdir]);
-    S2 = [];
-    for i = 1:length(S1)
-        if contains(S1(i).name, 'voltage')
-            S2 = [S2; S1(i)];
+    if nargin == 2
+        S1 = dir('./cache');
+        S1 = S1(~[S1.isdir]);
+        S2 = [];
+        for i = 1:length(S1)
+            if contains(S1(i).name, 'voltage')
+                S2 = [S2; S1(i)];
+            end
         end
+        [~, idx] = sort([S2.datenum]);
+        filename = ['./cache/' S2(idx(end)).name];
+    else
+        filename = varargin{1};
     end
-    [~, idx] = sort([S2.datenum]);
-    filename = ['./cache/' S2(idx(end)).name];
-else
-    filename = varargin{1};
-end
 
-data = load(filename);
-Tend = data.voltage.Time(end);
+    data = load(filename);
+    Tend = data.voltage.Time(end);
 
-mdl = 'playback_mech_explorer';
-load_system(mdl);
+    mdl = 'playback_mech_explorer';
+    load_system(mdl);
 
-set_param(mdl, 'StopTime', num2str(Tend));
+    set_param(mdl, 'StopTime', num2str(Tend));
 
-mdlws = get_param(mdl, 'ModelWorkspace');
-setVariablePart(mdlws, 'pendulum_type.Value', pendulum_type);
-setVariablePart(mdlws, 'obstacle_enable.Value', obstacle_enable);
-set_param([mdl '/From File'], 'FileName', filename);
+    mdlws = get_param(mdl, 'ModelWorkspace');
+    setVariablePart(mdlws, 'pendulum_type.Value', pendulum_type);
+    setVariablePart(mdlws, 'obstacle_enable.Value', obstacle_enable);
+    set_param([mdl '/From File'], 'FileName', filename);
 
-set_param(mdl, 'SimulationCommand', 'Update');
-pause(3);
+    set_param(mdl, 'SimulationCommand', 'Update');
+    pause(3);
 
-sim(mdl);
-pause(3);
+    sim(mdl);
+    pause(3);
 
-close_system(mdl, 0);
+    close_system(mdl, 0);
 
 end
